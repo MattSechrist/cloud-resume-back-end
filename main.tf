713d2242fac35f4607579717735244bde2da144b
MattSechrist <matthew.sechrist@gmail.com>
2021-09-10 22:45:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 terraform {
-    backend "remote" {
-        # The name of your Terraform Cloud organization.
-        organization = "matthewsechrist"
+  backend "remote" {
+    organization = "matthewsechrist"
 
-        # The name of the Terraform Cloud workspace to store Terraform state files in.
-        workspaces {
-          name = "cloud_resume_back_end"
-        }          
+    workspaces {
+      name = "cloud_resume_back_end"
     }
+  }
 }
