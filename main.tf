22ebe9982b5099735de1c2e7308178e21f398ee0
MattSechrist <matthew.sechrist@gmail.com>
2021-09-10 22:22:45 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 terraform {
-      backend "remote" {
+    backend "remote" {
         # The name of your Terraform Cloud organization.
         organization = "matthewsechrist"
 
         # The name of the Terraform Cloud workspace to store Terraform state files in.
         workspaces {
           name = "cloud_resume_back_end"
-        }
-      }
+        }          
     }
+}
 
-    # An example resource that does nothing.
-    resource "null_resource" "example" {
-      triggers = {
+# An example resource that does nothing.
+resource "null_resource" "example" {
+    triggers = {
         value = "A example resource that does nothing!"
-      }
     }
+}
