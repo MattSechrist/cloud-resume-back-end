516f29398de6480e59ff69a713f408ad7debbb7f
MattSechrist <matthew.sechrist@gmail.com>
2021-09-12 14:15:06 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,34 +20,14 @@ terraform {
   }
 }
 
-# S3 bucket for website.
-resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.bucket_name}"
-  acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "${var.bucket_name}" })
-
-  cors_rule {
-    allowed_headers = ["Authorization", "Content-Length"]
-    allowed_methods = ["GET", "POST"]
-    allowed_origins = ["https://${var.domain_name}"]
-    max_age_seconds = 3000
-  }
-
-  website {
-    index_document = "index.html"
-  }
-
-  tags = var.common_tags
-}
-
 # S3 bucket for redirecting non-www to www.
 resource "aws_s3_bucket" "www_root_bucket" {
-  bucket = var.bucket_name
+  bucket = "www.matthewsechrist.cloud"
   acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "www.${var.bucket_name}" })
+  policy = templatefile("templates/s3-policy.json", { bucket = "www.matthewsechrist.cloud" })
 
   website {
-    redirect_all_requests_to = "https://${var.domain_name}"
+    redirect_all_requests_to = "https://matthewsechrist.cloud"
   }
 
   tags = var.common_tags
