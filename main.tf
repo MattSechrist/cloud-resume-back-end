527db709855a6ec0205f7b58a1062203baadfa94
MattSechrist <matthew.sechrist@gmail.com>
2021-09-12 13:23:15 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,3 +7,23 @@ terraform {
     }
   }
 }
+
+resource "aws_s3_bucket" "main_bucket" {
+  bucket = "${var.bucket_name}"
+  acl    = "public-read"
+  policy = templatefile("templates/s3-policy.json", { bucket = "${var.bucket_name}" })
+
+  cors_rule {
+    allowed_headers = ["Authorization", "Content-Length"]
+    allowed_methods = ["GET", "POST"]
+    allowed_origins = ["https://${var.domain_name}"]
+    max_age_seconds = 3000
+  }
+
+  website {
+    index_document = "index.html"
+  }
+
+  tags = var.common_tags
+}
+
