84c97dfb40e6c2d25282079d5049f395696bebb0
MattSechrist <matthew.sechrist@gmail.com>
2021-09-12 11:09:36 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,3 @@ terraform {
     }
   }
 }
-resource "aws_s3_bucket" "www_main_bucket" {
-  bucket = var.bucket_name
-  acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
-
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
-  }
-
-  tags = var.common_tags
-}
