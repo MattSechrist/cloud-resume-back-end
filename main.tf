9caa7d2b2c660aad9ed60c20b997f1615be1d52e
MattSechrist <matthew.sechrist@gmail.com>
2021-09-11 21:07:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 terraform {
   
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }  
-  
   backend "remote" {
     organization = "matthewsechrist"
 
