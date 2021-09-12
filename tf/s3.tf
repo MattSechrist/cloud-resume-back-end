da4962397cee8abe6faef420e25741cc36c4f50a
MattSechrist <matthew.sechrist@gmail.com>
2021-09-12 10:39:57 -0400

    Update s3.tf

--- tf/s3.tf
+++ tf/s3.tf
@@ -1,32 +1,32 @@
 # S3 bucket for website.
-resource "aws_s3_bucket" "main_bucket" {
-  bucket = "${var.bucket_name}"
-  acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "${var.bucket_name}" })
-
-  cors_rule {
-    allowed_headers = ["Authorization", "Content-Length"]
-    allowed_methods = ["GET", "POST"]
-    allowed_origins = ["https://${var.domain_name}"]
-    max_age_seconds = 3000
-  }
-
-  website {
-    index_document = "index.html"
-  }
-
-  tags = var.common_tags
-}
-
-# S3 bucket for redirecting www to non-www.
-resource "aws_s3_bucket" "www_main_bucket" {
-  bucket = var.bucket_name
-  acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
-
-  website {
-    redirect_all_requests_to = "https://www.${var.domain_name}"
-  }
-
-  tags = var.common_tags
-}
+#resource "aws_s3_bucket" "main_bucket" {
+#  bucket = "${var.bucket_name}"
+#  acl    = "public-read"
+#  policy = templatefile("templates/s3-policy.json", { bucket = "${var.bucket_name}" })
+#
+#  cors_rule {
+#    allowed_headers = ["Authorization", "Content-Length"]
+#    allowed_methods = ["GET", "POST"]
+#    allowed_origins = ["https://${var.domain_name}"]
+#    max_age_seconds = 3000
+#  }
+#
+#  website {
+#    index_document = "index.html"
+#  }
+#
+#  tags = var.common_tags
+#}
+#
+## S3 bucket for redirecting www to non-www.
+#resource "aws_s3_bucket" "www_main_bucket" {
+#  bucket = var.bucket_name
+#  acl    = "public-read"
+#  policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
+#
+#  website {
+#    redirect_all_requests_to = "https://www.${var.domain_name}"
+#  }
+#
+#  tags = var.common_tags
+#}
