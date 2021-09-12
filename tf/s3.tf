ba7c1b4e7dd370a8e00023d71d3c00647f73c705
MattSechrist <matthew.sechrist@gmail.com>
2021-09-11 22:26:03 -0400

    Update s3.tf

--- tf/s3.tf
+++ tf/s3.tf
@@ -1,13 +1,13 @@
 # S3 bucket for website.
-resource "aws_s3_bucket" "$${var.bucket_name}_bucket" {
-  bucket = "$${var.bucket_name}"
+resource "aws_s3_bucket" "main_bucket" {
+  bucket = "${var.bucket_name}"
   acl    = "public-read"
-  policy = templatefile("templates/s3-policy.json", { bucket = "$${var.bucket_name}" })
+  policy = templatefile("templates/s3-policy.json", { bucket = "${var.bucket_name}" })
 
   cors_rule {
     allowed_headers = ["Authorization", "Content-Length"]
     allowed_methods = ["GET", "POST"]
-    allowed_origins = ["https://$${var.domain_name}"]
+    allowed_origins = ["https://${var.domain_name}"]
     max_age_seconds = 3000
   }
 
@@ -19,13 +19,13 @@ resource "aws_s3_bucket" "$${var.bucket_name}_bucket" {
 }
 
 # S3 bucket for redirecting www to non-www.
-resource "aws_s3_bucket" "www.$${var.domain_name}" {
+resource "aws_s3_bucket" "www.main_bucket" {
   bucket = var.bucket_name
   acl    = "public-read"
   policy = templatefile("templates/s3-policy.json", { bucket = var.bucket_name })
 
   website {
-    redirect_all_requests_to = "https://www.$${var.domain_name}"
+    redirect_all_requests_to = "https://www.${var.domain_name}"
   }
 
   tags = var.common_tags
