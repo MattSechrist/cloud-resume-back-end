e68a5d4fcb0b2bfeac29cbd9ca8ea211767ca8f5
MattSechrist <matthew.sechrist@gmail.com>
2021-09-13 21:25:35 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -75,6 +75,28 @@ provider "aws" {
 #
 
 # S3 bucket for redirecting non-www to www.
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = var.www_domain_name
+
+policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "MYBUCKETPOLICY"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject",
+        Resource = "arn:aws:s3:::${var.www_bucket_name}/*",
+        Condition = {
+          StringEquals = {
+            "aws:${var.header_name}": "${var.header_value}"
+          }
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_s3_bucket" "www_bucket" {
   bucket = var.www_domain_name
   acl    = "public-read"
