61933b5cd62abbf3c97cd327076ea21870bf5434
MattSechrist <matthew.sechrist@gmail.com>
2021-09-14 14:29:11 -0400

    tf updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,22 @@
 variable "domain_name" {
   type        = string
   description = "The domain name for the website. (ex. example.com)"
+  sensitive   = true
 }
 variable "www_domain_name" {
   type        = string
   description = "The www.domain name for the website with www prefix . (ex. www.example.com)"
+  sensitive   = true
 }
 variable "bucket_name" {
   type        = string
   description = "The name of the bucket without the www, same as domain_name."
+  sensitive   = true
 }
 
 variable "common_tags" {
   description = "Common tags you want applied to all components, group by Project."
+  sensitive   = true
 }
 
 variable "header_name" {
