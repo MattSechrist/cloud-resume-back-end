b6360a91ba1c28934499aa8cbf5ec192bcd63054
MattSechrist <matthew.sechrist@gmail.com>
2021-09-17 22:38:12 -0400

    tf updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -35,3 +35,34 @@ variable "buckets" {
   type        = map(string)
   description = "A map of the buckets with and without www prefix"
 }
+
+variable "lambda_function_name" {
+  type        = map(string)
+  description = "The Lambda function name"
+  sensitive   = true
+}
+variable "lambda_s3_bucket" {
+  type        = map(string)
+  description = "The Lambda S3 bucket"
+  sensitive   = true
+}
+variable "lambda_s3_file" {
+  type        = map(string)
+  description = "The Lambda S3 archive file"
+  sensitive   = true
+}
+variable "lambda_runtime" {
+  type        = map(string)
+  description = "The Lambda runtime"
+  sensitive   = true
+}
+variable "lambda_handler" {
+  type        = map(string)
+  description = "The Lambda handler"
+  sensitive   = true
+}
+variable "lambda_iam_role" {
+  type        = map(string)
+  description = "The Lambda IAM role"
+  sensitive   = true
+}    
