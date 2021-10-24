e0bd86bcb9eb74c478b885da3f82e5aef1e16c10
MattSechrist <matthew.sechrist@gmail.com>
2021-10-23 21:44:59 -0400

    Update lambda_edge.tf

--- terraform/lambda_edge.tf
+++ terraform/lambda_edge.tf
@@ -2,3 +2,9 @@
 // lambda role
 // tie to cloudfront distribution
 // switch from ZIP uploaded to S3 code?
+
+// change vairables
+// change lambda function names
+// explain use of SSM parameters for clarity and security
+
+// 
